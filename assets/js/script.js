42412307634fc1bae29f391819fc8a013d28c22d
achmadadji <adjids58@gmail.com>
2025-02-06 09:22:48 +0700

    Adji Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,15 @@
-let currentIndex = 0;
-
-function moveSlide(direction) {
-    const slider = document.querySelector(".slider");
-    const cards = document.querySelectorAll(".card");
-    const totalCards = cards.length;
-    const cardWidth = cards[0].offsetWidth + 20; // Tambah margin
-
-    currentIndex += direction;
-
-    if (currentIndex < 0) {
-        currentIndex = totalCards - 1;
-    } else if (currentIndex >= totalCards) {
-        currentIndex = 0;
-    }
-
-    const offset = -currentIndex * cardWidth;
-    slider.style.transform = `translateX(${offset}px)`;
-}
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 3000,
+    },
+  });
+  
