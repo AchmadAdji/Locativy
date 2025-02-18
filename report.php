acac66c0b711da9742294a33c8a0561e8b295244
achmadadji <adjids58@gmail.com>
2025-02-18 22:42:54 +0700

    Update Report

--- report.php
+++ report.php
@@ -51,7 +51,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
       <!-- Custom CSS -->
-      <link rel="stylesheet" href="style.css">
       <link rel="stylesheet" href="assets/css/footer.css">
       <link rel="stylesheet" href="assets/css/form-report.css">
       <link rel="stylesheet" href="assets/css/navbar.css">
