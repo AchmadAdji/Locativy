ba04d7c39bdfad1669fc3d9c47e6ab7d62b96ba5
achmadadji <adjids58@gmail.com>
2025-02-18 22:42:06 +0700

    Update Report

--- report.php
+++ report.php
@@ -51,9 +51,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
       <!-- Custom CSS -->
-      <link rel="stylesheet" href="footer.css">
-      <link rel="stylesheet" href="form-report.css">
-      <link rel="stylesheet" href="navbar.css">
+      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="assets/css/footer.css">
+      <link rel="stylesheet" href="assets/css/form-report.css">
+      <link rel="stylesheet" href="assets/css/navbar.css">
       <!-- Google Font -->
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
